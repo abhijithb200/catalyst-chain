8a8ac5eff6140027ca18bb4607fdf2abe11e1685
abhijithb200 <abhijithb007m@gmail.com>
2022-08-11 22:43:31 +0530

    lan working

--- client.py
+++ client.py
@@ -4,4 +4,4 @@ from lib.p2p import Peer2Peer
 
 # Privkey,Pubkey,Address = create_key_pair()
 tnx = {'from':'1','to':'A','nonce':0,'amount':10}
-Peer2Peer.send(tnx,('127.0.0.5',6000))
+Peer2Peer.send(tnx,('13.127.160.25',6000))
